bb2595521b2b552029df21e359f0aa5116c34618
Alex <alexbri@ciencias.unam.mx>
2022-11-23 18:29:48 -0500

    modifique la funcion de encontrar picos para que ahora solo regrese el contdor

--- imageProcessor/noisyPeakFinder.py
+++ imageProcessor/noisyPeakFinder.py
@@ -1,16 +1,16 @@
 """
-    Function to find peaks in a graph.
+    Function to count peaks in a graph.
 
     Receives a list with the data from the graph and creates a new one with the moving average of the data, where
     each point represents the average of the 2 previous points, the current one and the 2 following ones.
 
     The procedure is to go through the moving average list and keep track of the highest point so far,
-    once the current point is less than the average of all the data, add the index of that point to the
-    answer and search the next peak (if any)
+    once the current point is less than the average of all the data, add one to the answer
+    and search the next peak (if any)
 """
 
-def findPeaks(signal):
-    smoothed_values = [] # Ar
+def count_peaks(signal):
+    smoothed_values = [] 
     for i in range(0,len(signal)):
         average = 0
         for j in range(-2,3):
@@ -18,18 +18,18 @@ def findPeaks(signal):
         smoothed_values.append(average / 5)
 
     baseline = sum(signal) / len(signal)
-    peak_indices = [] 
-    peak_index, peak_val = None, None
 
+    peak_index, peak_val = None, None
+    peaks_count = 0
     for index,val in enumerate(smoothed_values):
         if val > baseline:
             if not peak_val or val > peak_val:
                 peak_index,peak_val = index,val
         elif val < baseline and peak_val:
-            peak_indices.append(peak_index)
+            peaks_count += 1
             peak_index,peak_val = None,None
 
     if peak_index:
-        peak_indices.append(peak_index)
+        peaks_count += 1
 
-    return peak_indices
+    return peaks_count

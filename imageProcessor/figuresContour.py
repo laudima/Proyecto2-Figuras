c04cc6343e2d2a2fbc0eae3088dbfddd5aa10f7b
Alex <alexbri@ciencias.unam.mx>
2022-11-23 23:56:13 -0500

    Agregue la salida en el main

--- imageProcessor/figuresContour.py
+++ imageProcessor/figuresContour.py
@@ -4,18 +4,16 @@ import math
 import numpy as np
 from cv2 import Mat
 import imageProcessor.figuresFinder as ff
-import mathplotlib.pyplot as plt 
+import matplotlib.pyplot as plt 
 
-def get_centers(img : Mat):
+def get_distances(img : Mat, colors):
 
-    color_set = ff.get_colors(img)
-
-    centers =[]
+    centers = []
     distanceContour = []
 
-    for i in range(len(color_set)):
-        lower = np.array(ff.hex_to_bgr(color_set[i]))
-        upper = np.array(ff.hex_to_bgr(color_set[i]))
+    for i in range(len(colors)):
+        lower = np.array(ff.hex_to_bgr(colors[i]))
+        upper = np.array(ff.hex_to_bgr(colors[i]))
     
         mask = cv2.inRange(img, lower, upper)
         inv_mask = cv2.bitwise_not(mask)
@@ -23,24 +21,26 @@ def get_centers(img : Mat):
 
         sumX = 0.0
         sumY = 0.0
-
-        for point in contours[1]:
+        c = len(contours) - 1
+        for point in contours[c]:
             sumX += point[0][0]
             sumY += point[0][1]
 
-        averageX = sumX // len(contours[1])
-        averageY = sumY // len(contours[1])
-        distanceContour.append(distance(averageX,averageY,contours[1]))
+        averageX = sumX // len(contours[c])
+        averageY = sumY // len(contours[c])
+        distanceContour.append(distance(averageX,averageY,contours[c]))
         centers.append([averageX, averageY])
 
 
-    print(centers)
-    print(distanceContour[0])
+    #print(centers)
+    #print(distanceContour[0])
+    """
     cv2.imshow("mask", img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+    """
 
-    return centers
+    return distanceContour
 
 def distance(centerX,centerY,contours):
     distanceContour = []
--- main.py
+++ main.py
@@ -2,12 +2,24 @@ import imageProcessor.imageReader as ir
 import imageProcessor.figuresFinder as ff
 import imageProcessor.noisyPeakFinder as pf
 import imageProcessor.figuresContour as fc
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    print("Hello Mundo")
-    x = ir.read_bitmap("example_1.bmp")
-    # print(pf.count_peaks(array))
-    print(ff.get_colors(x))
-    print(fc.get_centers(x))
+    x = ir.read_bitmap("example_5.bmp")
+    colors = list(ff.get_colors(x))
+ 
+    distances = fc.get_distances(x,colors)
+
+    for i,color in enumerate(colors):
+        peaks = pf.count_peaks(distances[i])
+        if peaks <= 3:
+            print(color,"= T")
+        elif peaks == 4:
+            print(color,"= C")
+        elif peaks > 9:
+            print(color,"= O")
+        else:
+            print(color,"= X")
+   
 else:
     print("Hello not in main")

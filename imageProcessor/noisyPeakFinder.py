c790de3351226cafa93346ee9ec62f4a654ca3c9
Alex <alexbri@ciencias.unam.mx>
2022-11-23 20:27:07 -0500

    Implemente la funcion para encontrar picos en la grafica

--- imageProcessor/noisyPeakFinder.py
+++ imageProcessor/noisyPeakFinder.py
@@ -1,35 +1,24 @@
-"""
-    Function to count peaks in a graph.
-
-    Receives a list with the data from the graph and creates a new one with the moving average of the data, where
-    each point represents the average of the 2 previous points, the current one and the 2 following ones.
-
-    The procedure is to go through the moving average list and keep track of the highest point so far,
-    once the current point is less than the average of all the data, add one to the answer
-    and search the next peak (if any)
-"""
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.signal import find_peaks
 
 def count_peaks(signal):
-    smoothed_values = [] 
+    min_index = signal.index(min(signal))
+    signal = signal[min_index:] + signal[:min_index]
+    smoothed_values = []
+   
     for i in range(0,len(signal)):
         average = 0
         for j in range(-2,3):
-            average += signal[i+j] if i + j >= 0 and i + j < len(signal) else signal[i]
+            average += signal[(i+j) % len(signal)]
         smoothed_values.append(average / 5)
-
-    baseline = sum(signal) / len(signal)
-
-    peak_val = None
-    peaks_count = 0
-    for val in smoothed_values:
-        if val > baseline:
-            if not peak_val or val > peak_val:
-                peak_val = val
-        elif val < baseline and peak_val:
-            peaks_count += 1
-            peak_val = None
-
-    if peak_val:
-        peaks_count += 1
-
-    return peaks_count
+    
+    peaks, _ = find_peaks(smoothed_values, height=0)
+    mean = sum(smoothed_values) / len(smoothed_values)
+    peaks = [peak for peak in peaks if smoothed_values[peak] >= mean]
+    """
+    plt.plot(smoothed_values)
+    plt.plot(peaks, np.array(smoothed_values)[peaks], "x")
+    plt.show()
+    """
+    return len(peaks)
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import imageProcessor.imageReader as ir
 import imageProcessor.figuresFinder as ff
+import imageProcessor.noisyPeakFinder as pf
 if __name__ == '__main__':
     print("Hello Mundo")
     x = ir.read_bitmap("example_1.bmp")
+    # print(pf.count_peaks(array))
     print(ff.get_colors(x))
 else:
     print("Hello not in main")
